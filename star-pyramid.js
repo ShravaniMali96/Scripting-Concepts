12a5d5b2d6e465e03f68c30ac04408387f832b77
Shravani Mali <shravanimmali@gmail.com>
2026-02-09 15:30:55 +0530

    Add files via upload

---
+++ star-pyramid.js
@@ -0,0 +1,20 @@
+function printStarPyramid(levels) {
+    for (let i = 1; i <= levels; i++) {
+        let row = '';
+
+      
+        for (let j = 1; j <= levels - i; j++) {
+            row += ' ';
+        }
+
+        
+        for (let k = 1; k <= 2 * i - 1; k++) {
+            row += '*';
+        }
+
+        console.log(row);
+    }
+}
+
+
+printStarPyramid(5);
